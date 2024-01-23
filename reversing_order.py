67f1a0c8ffcc9b50e5f035738bf45b7d40af7f65
HannahGenerana <hannahgenerana@gmail.com>
2024-01-24 00:49:42 +0800

    pseudocode

---
+++ reversing_order.py
@@ -0,0 +1,14 @@
+# Exercise 11: Write a Program to extract each digit from an integer in the reverse order.
+# For example, If the given int is 7536, the output shall be “6 3 5 7“, with a space separating the digits.
+
+# pseudocode
+
+# ask the user to input a integer
+
+# create a function
+
+# string the parameter to reverse its order
+
+# create a loop for spacing
+
+# print the reverse order of the integer
